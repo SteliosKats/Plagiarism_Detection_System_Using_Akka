14d1e55179d44dae887e778d637366cf28ef292b
SteliosKats <redhat77@gmx.com>
2014-10-05 00:54:51 +0300

    First commit

--- src/main/scala/sample/hello/Fileindexer.scala
+++ src/main/scala/sample/hello/Fileindexer.scala
@@ -17,7 +17,9 @@ import akka.routing.RoundRobinRoutingLogic
  * Created by root on 9/30/14.
  */
 case class file_properties(filename :File,fileid :Int)
-case class routingmessages(fileline :String, counter :Int)
+case class routingmessages(fileline :String, counter :Int,ref_act :ActorRef)
+case class return_references(reference_array :IndexedSeq[String], line_num :Int)
+
 object FileIndexer{
   def main(args: Array[String]): Unit = {
    var path_filename=new File(" ")
@@ -40,7 +42,7 @@ class FileReceiver extends Actor{
 
       /* Creating A router to route "Workers" to ectract citations for each line of the file */
       val router ={
-         val routees=Vector.fill(5){
+            val routees=Vector.fill(5){
             val lineseparator=context.actorOf(Props[LineSeparator])
             context watch lineseparator
             ActorRefRoutee(lineseparator)
@@ -54,8 +56,13 @@ class FileReceiver extends Actor{
        //println("file's length is"+filename.length())
        var counter=0
        for(line <- Source.fromFile(filename).getLines()){
-          router.route(routingmessages(line,counter), sender())
+          counter+=1
+          router.route(routingmessages(line,counter,self), sender())
        }
+    case return_references (ref_array, line_num) =>
+
+      val mapped_refs :Map[String, Int]= Map(ref_array map{s => (s, line_num)} : _*)
+
 
     case _ =>
       println("I got nothing")
@@ -68,29 +75,37 @@ class FileReceiver extends Actor{
 class LineSeparator extends Actor with ActorLogging {
   def receive ={
 
-    case routingmessages(line,counter) =>
+    case routingmessages(line,counter,file_receiver_ref) =>
     val references_ar=for( i <-0 to (line.length()-1) if(line.charAt(i) == '[') )yield i
     val references_de=for( i <-0 to (line.length()-1) if(line.charAt(i) == ']') )yield i
-    if (!references_ar.isEmpty){
-     // println("references_ar.length:"+references_ar.length+"\t references_de.length:"+references_ar.length)
+
+    if (!references_ar.isEmpty || !references_de.isEmpty){
       if(references_ar.length==1 && references_de.isEmpty ) {
-        val new_reference_array=(line.substring(references_ar(references_ar.length -1),line.length()))
-        println(new_reference_array)
+        val new_reference_array=IndexedSeq[String] (line.substring(references_ar(references_ar.length -1),line.length()))
+        //println(new_reference_array)
+        file_receiver_ref.!(return_references(new_reference_array,counter))
+      }
+      else if(references_de.length==1 && references_ar.isEmpty){
+        val new_reference_array= IndexedSeq[String] (line.substring(0,references_de(references_de.length -1)+1))
+        //println(new_reference_array)
+        file_receiver_ref.!(return_references(new_reference_array,counter))
       }
       else if(references_ar.length.>(references_de.length) && !references_de.isEmpty){
         val reference_array=for(i <- 0 to (references_ar.length -2) )yield line.substring(references_ar(i),references_de(i)+1)   //references_ar.foreach(reference => line.substring(reference,references_de) )
         val new_reference_array=reference_array.++(line.substring(references_ar(references_ar.length -1),line.length()).split("[\r\n]+"))
-        println(references_ar(references_ar.length -1)+"\t line length:"+(line.length()-1))
-        println(new_reference_array)
+        //println(new_reference_array)
+        file_receiver_ref.!(return_references(new_reference_array,counter))
       }
       else if(references_ar.length.<(references_de.length)) {
         var reference_array=for( i <- 0 to (references_de.length -2) )yield line.substring(references_ar(i),references_de(i+1)+1)
         val new_reference_array=reference_array.++(line.substring(0,references_de(0)+1).split("[\r\n]+"))
-        println(new_reference_array)
+        //println(new_reference_array)
+        file_receiver_ref.!(return_references(new_reference_array,counter))
       }
       else{
-        val reference_array=for(i <- 0 to (references_ar.length-1) )yield line.substring(references_ar(i),references_de(i)+1)
-        println(reference_array)
+        val new_reference_array=for(i <- 0 to (references_ar.length-1) )yield line.substring(references_ar(i),references_de(i)+1)
+        //println(reference_array)
+        file_receiver_ref.!(return_references(new_reference_array,counter))
       }
 
     }
